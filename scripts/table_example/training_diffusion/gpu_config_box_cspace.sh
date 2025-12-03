7e251ac4a83aeeb84fa2b975d580a0c82d51f1a7
arthur <arthur.haffemayer@yahoo.fr>
2025-12-03 12:51:41 +0100

    training scripts for cspace

---
+++ scripts/table_example/training_diffusion/gpu_config_box_cspace.sh
@@ -0,0 +1,1 @@
+python ../../../src/conditional_diffusion_motion/diffusion_transformer/trainings/cspace/fit_indirect_motion_with_obstacles_with_box_cspace.py fit --config config/gpu_config_box_cspace.yaml
