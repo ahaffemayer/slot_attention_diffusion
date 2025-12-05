91f16bee745d6ad39c15973f786099190fb70dd4
arthur <arthur.haffemayer@yahoo.fr>
2025-12-05 14:14:45 +0100

    cspace script

---
+++ scripts/table_example/training_diffusion/gpu_config_drawer_cspace.sh
@@ -0,0 +1,1 @@
+python ../../../src/conditional_diffusion_motion/diffusion_transformer/trainings/cspace/fit_indirect_motion_with_obstacles_with_drawer_cspace.py fit --config config/gpu_config_drawer_cspace.yaml
